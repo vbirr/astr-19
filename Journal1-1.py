ba8bbcb709312f0d696fc64374e433dc19509bb3
Vanessa Birrueta <birrueta.vanessa@gmail.com>
2023-10-20 10:25:15 -0700

    Add files via upload

---
+++ Journal1-1.py
@@ -0,0 +1,22 @@
+"""
+Name: Vanessa Birrueta-Hernandez
+Journal #1 - Session 1 Prompt
+
+"""
+
+# This prints three lines
+def PrintDetails():
+    print('My name is Vanessa Birrueta-Hernandez.')
+    print('My favorite movie is "La Misma Luna"')
+    print('My favorite food is enchiladas.')
+
+# the main program that runs my other methods
+def main():
+    PrintDetails()
+
+#  run the main
+if __name__=="__main__":
+    main()
+
+
+
