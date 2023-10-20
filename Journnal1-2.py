4921ebeabdf04014bf9bf984a1c9c21de657c54a
Vanessa Birrueta <birrueta.vanessa@gmail.com>
2023-10-20 10:25:30 -0700

    Add files via upload

---
+++ Journnal1-2.py
@@ -0,0 +1,34 @@
+"""
+Name: Vanessa Birrueta-Hernandez
+Journal #1 - Session 2 Prompt
+
+"""
+
+# Prints the sum of two floating point numbers
+def Sum(a, b):
+    print(a + b)
+
+# Finds the Difference between two integers
+def Difference(a, b):
+    print(b - a)
+
+# Returns the product of a floating point number and an integer
+def Multiplication(a,b):
+    print(a * b)
+
+# the main program that runs my other methods
+def main():
+    a = 1.1
+    b = 2.2
+    c = 3
+    d = 4
+    Sum(a,b)
+    Difference(c,d)
+    Multiplication(a,c)
+
+#  run the main
+if __name__=="__main__":
+    main()
+
+
+
