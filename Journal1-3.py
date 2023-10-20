36d259495e1d822491560f280f844596bd11ed7c
Vanessa Birrueta <birrueta.vanessa@gmail.com>
2023-10-20 13:07:06 -0700

    Add files via upload

---
+++ Journal1-3.py
@@ -0,0 +1,26 @@
+"""
+Name: Vanessa Birrueta-Hernandez
+Journal #1 - Session 3 Prompt
+
+"""
+
+# defines function f(x) that returns x^3 + 8
+def Function(a):
+    return (a * a * a + 9)
+
+
+
+# the main program that runs my other methods
+def main():
+    value = Function(9)
+    print(value)
+
+    if value > 27:
+        print("YAY!")
+
+#  run the main
+if __name__== "__main__":
+    main()
+
+
+
